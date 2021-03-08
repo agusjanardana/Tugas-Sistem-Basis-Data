e5d1bdf9608d724214e40446dd6cc9e0d79e5923
Agus Janardana <62202561+agusjanardana@users.noreply.github.com>
2021-03-08 18:26:56 +0800

    Add files via upload

---
+++ Item.sql
@@ -0,0 +1,202 @@
+--  ITEMS
+
+INSERT INTO Items VALUES
+(1, Papayas, 578, Rp. 12000,00-);
+INSERT INTO Items VALUES
+(2, Beef - Tender Tips, 28, Rp. 168000,00-);
+INSERT INTO Items VALUES
+(3, Duck - Fat, 80, Rp. 45999,00-);
+INSERT INTO Items VALUES
+(4, Potatoes - Purple,  Organic, Rp. 120,00-);
+INSERT INTO Items VALUES
+(5, Coconut,  Sweet, Rp. 633,00-);
+INSERT INTO Items VALUES
+(6, Veal - Slab Bacon, 200, Rp. 35000,00-);
+INSERT INTO Items VALUES
+(7, Placemat - Scallop,  White, Rp. 111,00-);
+INSERT INTO Items VALUES
+(8, Flour - Strong Pizza, 80, Rp. 39999,00-);
+INSERT INTO Items VALUES
+(9, Milk - Skim, 500, Rp. 19000,00-);
+INSERT INTO Items VALUES
+(10, Leeks - Large, 200, Rp. 6000,00-);
+INSERT INTO Items VALUES
+(11, Container - Foam Dixie 12 Oz, 450, Rp. 11909,00-);
+INSERT INTO Items VALUES
+(12, Corn Shoots, 615, Rp. 22000,00-);
+INSERT INTO Items VALUES
+(13, Muffin - Zero Transfat, 340, Rp. 36000,00-);
+INSERT INTO Items VALUES
+(14, Sole - Dover,  Whole, Rp.  Fresh,00-);
+INSERT INTO Items VALUES
+(15, Jolt Cola - Electric Blue, 300, Rp. 17000,00-);
+INSERT INTO Items VALUES
+(16, Turkey - Whole,  Fresh, Rp. 20,00-);
+INSERT INTO Items VALUES
+(17, Tea - Mint, 317, Rp. 18000,00-);
+INSERT INTO Items VALUES
+(18, Coriander - Seed, 340, Rp. 10000,00-);
+INSERT INTO Items VALUES
+(19, Bok Choy - Baby, 120, Rp. 14000,00-);
+INSERT INTO Items VALUES
+(20, Wine - Fontanafredda Barolo, 300, Rp. 70000,00-);
+INSERT INTO Items VALUES
+(21, Wine - Red,  Wolf Blass, Rp.  Yellow,00-);
+INSERT INTO Items VALUES
+(22, Snapple - Iced Tea Peach, 200, Rp. 28000,00-);
+INSERT INTO Items VALUES
+(23, Cake - Miini Cheesecake Cherry, 195, Rp. 38000,00-);
+INSERT INTO Items VALUES
+(24, Melon - Cantaloupe, 80, Rp. 24000,00-);
+INSERT INTO Items VALUES
+(25, Tea - Herbal I Love Lemon, 133, Rp. 49000,00-);
+INSERT INTO Items VALUES
+(26, Bandage - Flexible Neon, 515, Rp. 7000,00-);
+INSERT INTO Items VALUES
+(27, Wine - Cahors Ac 2000,  Clos, Rp. 80,00-);
+INSERT INTO Items VALUES
+(28, Beer - Sleemans Cream Ale, 160, Rp. 90000,00-);
+INSERT INTO Items VALUES
+(29, Sultanas, 670, Rp. 32000,00-);
+INSERT INTO Items VALUES
+(30, Truffle - Whole Black Peeled, 120, Rp. 18000,00-);
+INSERT INTO Items VALUES
+(31, Oil - Canola, 80, Rp. 90000,00-);
+INSERT INTO Items VALUES
+(32, Wild Boar - Tenderloin, 135, Rp. 250000,00-);
+INSERT INTO Items VALUES
+(33, Fish - Bones120, 30500, Rp. None,00-);
+INSERT INTO Items VALUES
+(34, Tequila Rose Cream Liquor, 355, Rp. 150000,00-);
+INSERT INTO Items VALUES
+(35, Eggroll, 150, Rp. 22000,00-);
+INSERT INTO Items VALUES
+(36, Lid 16 Oz, 144, Rp. 80000,00-);
+INSERT INTO Items VALUES
+(37, Sproutsmustard Cress, 324, Rp. 48000,00-);
+INSERT INTO Items VALUES
+(38, Creme De Cacao Mcguines, 300, Rp. 56000,00-);
+INSERT INTO Items VALUES
+(39, Haggis, 200, Rp. 36000,00-);
+INSERT INTO Items VALUES
+(40, Longan, 250, Rp. 20000,00-);
+INSERT INTO Items VALUES
+(41, Sauce Bbq Smokey, 382, Rp. 28000,00-);
+INSERT INTO Items VALUES
+(42, Godiva White Chocolate, 240, Rp. 19000,00-);
+INSERT INTO Items VALUES
+(43, Trout - Rainbow,  Frozen, Rp. 300,00-);
+INSERT INTO Items VALUES
+(44, Banana - Green, 500, Rp. 27000,00-);
+INSERT INTO Items VALUES
+(45, Curry Powder Madras, 377, Rp. 8000,00-);
+INSERT INTO Items VALUES
+(46, Bread - Italian Roll With Herbs, 80, Rp. 12000,00-);
+INSERT INTO Items VALUES
+(47, Peach - Fresh, 210, Rp. 22000,00-);
+INSERT INTO Items VALUES
+(48, Chinese Foods - Plain Fried Rice, 340, Rp. 47700,00-);
+INSERT INTO Items VALUES
+(49, Ostrich - Prime Cut, 750, Rp. 10462,00-);
+INSERT INTO Items VALUES
+(50, Flour - Pastry, 53, Rp. 47714,00-);
+INSERT INTO Items VALUES
+(51, Lamb - Bones, 720, Rp. 33631,00-);
+INSERT INTO Items VALUES
+(52, Juice - Tomato,  48 Oz, Rp. 590,00-);
+INSERT INTO Items VALUES
+(53, 'Salmon - Whole,  4 - 6 Pounds', Rp. 569,00-);
+INSERT INTO Items VALUES
+(54, Pecan Raisin - Tarts, 121, Rp. 25159,00-);
+INSERT INTO Items VALUES
+(55, Tequila - Sauza Silver, 620, Rp. 31166,00-);
+INSERT INTO Items VALUES
+(56, Jolt Cola - Electric Blue, 626, Rp. 23563,00-);
+INSERT INTO Items VALUES
+(57, Neckerchief Blck, 761, Rp. 25741,00-);
+INSERT INTO Items VALUES
+(58, Tomato Paste, 192, Rp. 38281,00-);
+INSERT INTO Items VALUES
+(59, Pepper - Green, 39, Rp. 27910,00-);
+INSERT INTO Items VALUES
+(60, Potato - Sweet, 646, Rp. 5843,00-);
+INSERT INTO Items VALUES
+(61, Apple - Fuji, 171, Rp. 24786,00-);
+INSERT INTO Items VALUES
+(62, Pork - European Side Bacon, 312, Rp. 32735,00-);
+INSERT INTO Items VALUES
+(63, 'Nut - Pecan,  Pieces', Rp. 396,00-);
+INSERT INTO Items VALUES
+(64, Bread - Triangle White, 406, Rp. 24553,00-);
+INSERT INTO Items VALUES
+(65, 'Pork - Tenderloin,  Fresh', Rp. 237,00-);
+INSERT INTO Items VALUES
+(66, 'Chicken - Wings,  Tip Off', Rp. 363,00-);
+INSERT INTO Items VALUES
+(67, Bagel - Whole White Sesame, 143, Rp. 36856,00-);
+INSERT INTO Items VALUES
+(68, 'Soup - French Onion,  Dry', Rp. 157,00-);
+INSERT INTO Items VALUES
+(69, Coffee Guatemala Dark, 125, Rp. 48723,00-);
+INSERT INTO Items VALUES
+(70, Sauce - Rosee, 337, Rp. 47269,00-);
+INSERT INTO Items VALUES
+(71, Kahlua, 686, Rp. 30605,00-);
+INSERT INTO Items VALUES
+(72, Water - Green Tea Refresher, 128, Rp. 45198,00-);
+INSERT INTO Items VALUES
+(73, Bread - Italian Roll With Herbs, 306, Rp. 22017,00-);
+INSERT INTO Items VALUES
+(74, 'Coke - Classic,  355 Ml', Rp. 573,00-);
+INSERT INTO Items VALUES
+(75, Bar Mix - Lime, 8, Rp. 6767,00-);
+INSERT INTO Items VALUES
+(76, Sauce - Vodka Blush, 344, Rp. 39705,00-);
+INSERT INTO Items VALUES
+(77, Pastry - Apple Muffins - Mini, 71, Rp. 26790,00-);
+INSERT INTO Items VALUES
+(78, Veal - Loin, 740, Rp. 29506,00-);
+INSERT INTO Items VALUES
+(79, 'Beef - Roasted,  Cooked', Rp. 130,00-);
+INSERT INTO Items VALUES
+(80, Plate Foam Laminated 9in Blk, 758, Rp. 4990,00-);
+INSERT INTO Items VALUES
+(81, 'Lettuce - Mini Greens,  Whole', Rp. 156,00-);
+INSERT INTO Items VALUES
+(82, Glass Clear 7 Oz Xl, 397, Rp. 2902,00-);
+INSERT INTO Items VALUES
+(83, Papadam, 448, Rp. 34998,00-);
+INSERT INTO Items VALUES
+(84, Chocolate - Mi - Amere Semi, 440, Rp. 49640,00-);
+INSERT INTO Items VALUES
+(85, 'Oregano - Dry,  Rubbed', Rp. 678,00-);
+INSERT INTO Items VALUES
+(86, Sprite - 355 Ml, 599, Rp. 23867,00-);
+INSERT INTO Items VALUES
+(87, 'Juice - Apple,  1.36l', Rp. 223,00-);
+INSERT INTO Items VALUES
+(88, 'Noodles - Cellophane,  Thin', Rp. 751,00-);
+INSERT INTO Items VALUES
+(89, Cauliflower, 81, Rp. 26303,00-);
+INSERT INTO Items VALUES
+(90, Dasheen, 683, Rp. 48407,00-);
+INSERT INTO Items VALUES
+(91, Sprouts - Brussel, 672, Rp. 2118,00-);
+INSERT INTO Items VALUES
+(92, Cake - Mini Cheesecake, 558, Rp. 17996,00-);
+INSERT INTO Items VALUES
+(93, Amarula Cream, 285, Rp. 3552,00-);
+INSERT INTO Items VALUES
+(94, 'Tea - Darjeeling,  Azzura', Rp. 8,00-);
+INSERT INTO Items VALUES
+(95, Capon - Whole, 361, Rp. 4664,00-);
+INSERT INTO Items VALUES
+(96, Langers - Ruby Red Grapfruit, 131, Rp. 38107,00-);
+INSERT INTO Items VALUES
+(97, Sping Loaded Cup Dispenser, 250, Rp. 17976,00-);
+INSERT INTO Items VALUES
+(98, Bread, 125, Rp. 44736,00-);
+INSERT INTO Items VALUES
+(99, Rabbit - Legs, 177, Rp. 37933,00-);
+INSERT INTO Items VALUES
+(100, Skirt - 24 Foot, 165, Rp. 41729,00-);
