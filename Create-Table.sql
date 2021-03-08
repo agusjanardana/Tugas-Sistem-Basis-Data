130d00aa8fd2d3b0d696d0b2f46dce1ab48a7719
Agus Janardana <62202561+agusjanardana@users.noreply.github.com>
2021-03-08 17:52:11 +0800

    Add files via upload

---
+++ Create-Table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Costumer (
+    ID_Costumer         int NOT NULL AUTO_INCREMENT,
+    Costumer_Name       VARCHAR(50),
+    Phone_Number        INT(10),
+    Costumer_Address    VARCHAR(50),
+    PRIMARY KEY(ID_Costumer)
+);
+
+CREATE TABLE Items (
+    ID_Items        int NOT NULL AUTO_INCREMENT,
+    Item_Name       VARCHAR(50),
+    Item_Price      int,
+    Item_Stock      int,
+    PRIMARY KEY(ID_Items)
+);
+
+CREATE TABLE Transaction (
+    ID_Transaksi    int NOT NULL AUTO_INCREMENT,
+    ID_Items        int NOT NULL,
+    ID_Costumer     int NOT NULL,
+    Details_Waktu   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Keterangan      enum("processed","sukses","gagal","cancel") DEFAULT "processed",
+
+    PRIMARY KEY (ID_Transaksi),
+    FOREIGN KEY (ID_Items) REFERENCES Items(ID_Items) ON DELETE CASCADE,
+    FOREIGN KEY (ID_Costumer) REFERENCES Costumer(ID_Costumer) ON DELETE CASCADE
+);
